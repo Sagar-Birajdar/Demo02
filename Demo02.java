18180ea265e8080aecb22c3fac94c456d0454621
Sagar Birajdar <birajdarsagar5991@gmail,com>
2023-07-15 11:34:51 +0530

    File is ready to comit

---
+++ Demo02.java
@@ -0,0 +1,18 @@
+class Demo{
+    int i = 20;
+    int j = 20;
+
+    int c = i+j;
+
+   void add() {
+        System.out.println(c);
+    }
+
+public static void main(String[] args) {
+    
+ Demo d = new Demo();
+ d.add();
+
+}
+    
+}
